8e3064cf6623e0f5817933a3ebfe4beb7e30c28d
jonahneugeboren <jonahneugeboren@10.251.213.182>
2015-07-28 14:06:44 -0400

    Helper classes

---
+++ JAuth/InitialSaveWriter.java
@@ -0,0 +1,7 @@
+package JAuth;
+
+public class InitialSaveWriter {
+	public static void main (String [] args) {
+		AuthenticatorGUI aGui = new AuthenticatorGUI();
+	}
+}
---
+++ JAuth/MyKeyListener.java
@@ -0,0 +1,51 @@
+package JAuth;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MyKeyListener implements KeyListener{
+	public AuthenticatorGUI aGui;
+	
+	public MyKeyListener(AuthenticatorGUI aGui) {
+		this.aGui = aGui;
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if(aGui.firstFrame.isVisible()) {
+				if(!aGui.newPass.getText().equals("") && !aGui.newPass.getText().equals(" ")) {
+					aGui.password = aGui.newPass.getText();
+					aGui.firstFrame.dispose();
+					aGui.setVisible(true);
+					aGui.edit();
+	    	}
+			}
+			else if(aGui.table.isVisible()) {
+				aGui.save();
+			    aGui.table.dispose();
+			}
+			else if(aGui.frame.isVisible()) {
+				  String passTry = aGui.pass.getText();
+				  if(passTry.equals(aGui.password)) {
+		    		aGui.checkPass = true;
+					aGui.frame.dispose();
+					aGui.edit();
+		    	}  
+			} 
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
